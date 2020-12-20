975541c86bd2e394d859c29988d08a86bd7a4724
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-20 17:43:51 +0900

    不要ファイル未生成設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,13 @@ module ProtectiveCatCrowdfunding
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    #生成したファイルに対応したスタイルシート・ヘルパー・Javascript・アプリケーションをテストするためのファイルが同時に生成されないようにする
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
