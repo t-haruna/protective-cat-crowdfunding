202d0dddec54734880f9789d668a5a726e84a35b
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-22 19:26:52 +0900

    バリデーション設定

---
+++ app/models/project.rb
@@ -0,0 +1,13 @@
+class Project < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :profile
+    validates :target_amount
+    validates :tarm
+    validates :return_title_1
+    validates :return_price_1
+  end
+
+  belongs_to :user
+end
