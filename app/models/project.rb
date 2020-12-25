25edcb7867c58ae23c8cdfe17d73db6ac31a7683
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-25 18:19:12 +0900

    追加カラムのバリデーション設定

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,18 @@
 class Project < ApplicationRecord
   with_options presence: true do
-    validates :title
-    validates :text
-    validates :profile
-    validates :target_amount
-    validates :tarm
-    validates :return_title_1
-    validates :return_price_1
+    validates :title,
+      length: {maximum: 40},
+      presence: true
+    validates :text, presence: true
+    validates :profile, presence: true
+    validates :target_amount, presence: true
+    validates :tarm, presence: true
+    validates :return_title_1, presence: true
+    validates :return_price_1, 
+    numericality: {only_integer: true, greater_than_or_equal_to: 500, less_than_or_equal_to: 100000},
+    presence: true
   end
 
   belongs_to :user
+  mount_uploader :image, ImageUploader
 end
