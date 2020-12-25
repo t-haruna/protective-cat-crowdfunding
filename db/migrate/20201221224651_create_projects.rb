3c979a596912b4571ed724855492ca9326827035
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-25 18:17:47 +0900

    画像カラムの追加

--- db/migrate/20201221224651_create_projects.rb
+++ db/migrate/20201221224651_create_projects.rb
@@ -6,14 +6,18 @@ class CreateProjects < ActiveRecord::Migration[6.0]
       t.text :image,              null: false
       t.text :text,               null: false
       t.text :profile,            null: false
+      t.text :profile_image,       null: false
       t.integer :target_amount,   null: false
       t.date :tarm,               null: false
       t.string  :return_title_1,  null: false
       t.integer :return_price_1,  null: false
+      t.text :return_image_1,      null: false
       t.string  :return_title_2
       t.integer :return_price_2
+      t.text :return_image_2
       t.string  :return_title_3
       t.integer :return_price_3
+      t.text :return_image_3
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,14 +18,18 @@ ActiveRecord::Schema.define(version: 2020_12_21_224651) do
     t.text "image", null: false
     t.text "text", null: false
     t.text "profile", null: false
+    t.text "profile_image", null: false
     t.integer "target_amount", null: false
     t.date "tarm", null: false
     t.string "return_title_1", null: false
     t.integer "return_price_1", null: false
+    t.text "return_image_1", null: false
     t.string "return_title_2"
     t.integer "return_price_2"
+    t.text "return_image_2"
     t.string "return_title_3"
     t.integer "return_price_3"
+    t.text "return_image_3"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
