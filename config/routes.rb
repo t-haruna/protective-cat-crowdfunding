b9251a4049e8908cad2add5676ab667c5341cf9f
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-27 12:23:54 +0900

    下書き保存のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "projects#index"
-  resources :projects, only: [:index, :new, :create, :show] 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects, only: [:index, :new, :create, :show] do
+    resources :drafts, only: [:new, :create ]
+  end
+  
 end
