0eb880429f2b5511ef3587c2f4265578cd53a030
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-29 08:10:43 +0900

    mypageルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "projects#index"
-  resources :projects, only: [:index, :new, :create, :show] do
+  resources :projects do
+    collection do
+      get 'mypage'
+      get 'draft_display'
+      get 'posting_project'
+      get 'done_project'
+    end
     resources :drafts, only: [:new, :create ]
   end
   
