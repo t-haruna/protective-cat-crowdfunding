2a39e991c052f55f3db173a8ea5d8d7f628e75cd
t-haruna <68949701+t-haruna@users.noreply.github.com>
2020-12-31 17:00:12 +0900

    turbolinksを外す

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,13 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")  // ←の1行を追加
 require("project_new.js")  // ←の1行を追加
 require("billing_new.js")  // ←の1行を追加
+require("payjp.js")  // ←の1行を追加
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
