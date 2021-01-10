ffac67fa161a8d1c39d3d604bbe84340ee59825f
t-haruna <68949701+t-haruna@users.noreply.github.com>
2021-01-10 18:18:03 +0900

    クレカ修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,7 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("pk_test_0b9a54801699111259cd63e8"); //ここに公開鍵を直書き
+      Payjp.setPublicKey("pk_test_bd34a37047418348d799b0df"); //ここに公開鍵を直書き
       let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
       btn.addEventListener("click", e => { //ボタンが押されたときに作動します
         e.preventDefault(); //ボタンを一旦無効化します
