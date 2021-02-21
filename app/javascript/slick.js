5a1111114f07e89d5257bc53d023deae564d374d
t-haruna <68949701+t-haruna@users.noreply.github.com>
2021-02-21 17:22:43 +0900

    slick OFF

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,9 +1,9 @@
-$(function() {
-  $('.Phot').slick({
-      dots: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      pauseOnFocus: false
+// $(function() {
+//   $('.Phot').slick({
+//       dots: true,
+//       autoplay: true,
+//       autoplaySpeed: 3000,
+//       pauseOnFocus: false
       
-  });
-});
+//   });
+// });
