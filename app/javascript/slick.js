d93a5f2f44bcb2d2677a3fc25ba414e80b8dee37
t-haruna <68949701+t-haruna@users.noreply.github.com>
2021-03-09 13:48:28 +0900

    slick修正

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,9 +1,18 @@
-// $(function() {
-//   $('.Phot').slick({
-//       dots: true,
-//       autoplay: true,
-//       autoplaySpeed: 3000,
-//       pauseOnFocus: false
+$(function() {
+  $('.Phot').slick({
+      // dots: true,
+    //autoplay: true,
+      // fade: true,
+      // cssEase: 'linear',
+    autoplaySpeed: 5000,
+    pauseOnFocus: false,
       
-//   });
-// });
+    dots: true,
+    infinite: true,
+    fade: true,
+    speed: 3000,
+    
+    //slide: 'div',
+    cssEase: 'linear'
+  });
+});
