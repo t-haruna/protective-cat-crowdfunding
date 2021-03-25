3e6c23c11775763ac9e4852180333df993ae5044
t-haruna <68949701+t-haruna@users.noreply.github.com>
2021-03-25 15:35:59 +0900

    トップ画面調整

--- app/javascript/scrollreveal.js
+++ app/javascript/scrollreveal.js
@@ -4,41 +4,48 @@ ScrollReveal().reveal('.Tag',{
   duration: 2000, // アニメーション完了にかかる時間
   origin: 'left', // 要素がどの方向から来るか
   distance: '500px', // 移動する距離
-  reset: true // フレームインの度に動かすか
+  reset: false // フレームインの度に動かすか
   });
 ScrollReveal().reveal('.Contents__expect__title',{
   delay: 300, // アニメーション開始までの時間
   duration: 2000, // アニメーション完了にかかる時間
   origin: 'bottom', // 要素がどの方向から来るか
   distance: '50px', // 移動する距離
-  reset: true // フレームインの度に動かすか
+  reset: false // フレームインの度に動かすか
   });
 ScrollReveal().reveal('.Contents__expect p',{
   delay: 300, // アニメーション開始までの時間
   duration: 2000, // アニメーション完了にかかる時間
   origin: 'bottom', // 要素がどの方向から来るか
   distance: '50px', // 移動する距離
-  reset: true // フレームインの度に動かすか
+  reset: false // フレームインの度に動かすか
   });
 ScrollReveal().reveal('.title-line',{
   delay: 1500, // アニメーション開始までの時間
   duration: 1000, // アニメーション完了にかかる時間
   origin: 'left', // 要素がどの方向から来るか
   distance: '1500px', // 移動する距離
-  reset: true
+  reset: false
+  }); 
+ScrollReveal().reveal('.Btn-box-2',{
+  delay: 300, // アニメーション開始までの時間
+  duration: 2000, // アニメーション完了にかかる時間
+  origin: 'bottom', // 要素がどの方向から来るか
+  distance: '50px', // 移動する距離
+  reset: false
   }); 
 ScrollReveal().reveal('.Projects',{
   delay: 300, // アニメーション開始までの時間
   duration: 1600, // アニメーション完了にかかる時間
   origin: 'bottom', // 要素がどの方向から来るか
   distance: '50px', // 移動する距離
-  reset: true // フレームインの度に動かすか
+  reset: false // フレームインの度に動かすか
   });
 ScrollReveal().reveal('.Achievement',{
   delay: 300, // アニメーション開始までの時間
   duration: 1600, // アニメーション完了にかかる時間
   origin: 'bottom', // 要素がどの方向から来るか
   distance: '50px', // 移動する距離
-  reset: true // フレームインの度に動かすか
+  reset: false // フレームインの度に動かすか
     });
 });
